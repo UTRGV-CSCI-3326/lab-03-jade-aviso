a34a3761935ee5cf6746db5596ea99a5d35029c0
Jade Sapphire D. Aviso <147261623+jade-aviso@users.noreply.github.com>
2025-01-30 17:45:07 +0000

    Round the final total to 2 decimal places

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,32 @@
 
+public class BankAccount{
+    public static void main(String [] args){
+    double BobAccount = 2175.37;
+
+    //Withdrew 302.50
+    BobAccount -= 302.50;
+
+    //Deposit 50.03
+    BobAccount += 50.03;    
+
+    //Withdrew half of current balance
+    //deposited 20.00
+    BobAccount /= 2;
+    BobAccount += 20;
+
+    //withdrew 1
+    BobAccount--;
+
+
+    //deposited double his balance
+    BobAccount *= 2;
+
+    //deposit 1
+    BobAccount++;
+
+    //Outputs Bob's bank statement
+    System.out.println("Current Balance is :$" + BobAccount + "." );
+
+    }
+
+}
